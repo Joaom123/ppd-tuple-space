082d8de54cf458169140381c6ee968c6f91a9d81
João Marcus <joaomarcus12769@gmail.com>
2022-05-19 23:52:17 -0300

    protótipo das telas

--- src/main/java/ifce/ppd/tuplespace/controller/InitController.java
+++ src/main/java/ifce/ppd/tuplespace/controller/InitController.java
@@ -2,13 +2,24 @@ package ifce.ppd.tuplespace.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class InitController {
     @FXML
-    private Label welcomeText;
+    private TextField usernameInput;
+
+    @FXML
+    private Text usernameFieldErrorMessage;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        if(usernameInput.getText() == null || usernameInput.getText().trim().isEmpty()) {
+            usernameFieldErrorMessage.setText("O nome de usuário é obrigatório");
+            return;
+        }
+
+        usernameFieldErrorMessage.setText("");
+        // Go to choose room view
     }
 }
